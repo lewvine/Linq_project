d39fc15a0b582ebd5a3a6c3678470892fe2c1b38
Lewis Vine <lew.vine@gmail.com>
2021-03-11 12:05:32 -0500

    First problem draft

--- PracticeProblemsLINQ/LinqProblems.cs
+++ PracticeProblemsLINQ/LinqProblems.cs
@@ -13,79 +13,88 @@ namespace PracticeProblemsLINQ
        
         #region Problem 1 
         //(5 points) Problem 1
-        //Using LINQ, write a method that takes in a list of strings and returns all words that contain the substring “th” from a list.
+        //Using LINQ, write a method that takes in a list of strings and returns 
+        //all words that contain the substring “th” from a list.
         public static List<string> RunProblem1(List<string> words)
         {
             //code
-
+            var result = words.Where(w => w.Contains("th"));
+            List<string> newList = new List<string>();
+            foreach (var word in result)
+            {
+                Console.WriteLine(word);
+                Console.ReadLine();
+                newList.Add(word);
+            }
+            return newList;
             //return
 
         }
         #endregion
 
-        #region Problem 2 
-        //(5 points) Problem 2
-        //Using LINQ, write a method that takes in a list of strings and returns a copy of the list without duplicates.
-        public static List<string> RunProblem2(List<string> names)
-        {
-            //code
+        //#region Problem 2 
+        ////(5 points) Problem 2
+        ////Using LINQ, write a method that takes in a list of strings and returns a copy of the list without duplicates.
+        //public static List<string> RunProblem2(List<string> names)
+        //{
+        //    //code
 
-            //return
+        //    //return
 
-        }
-        #endregion
+        //}
+        //#endregion
 
-        #region Problem 3
-        //(5 points) Problem 3
-        //Using LINQ, write a method that takes in a list of customers and returns the lone customer who has the name of Mike. 
-        public static Customer RunProblem3(List<Customer> customers)
-        {
-            //code
+        //#region Problem 3
+        ////(5 points) Problem 3
+        ////Using LINQ, write a method that takes in a list of customers and returns the lone customer who has the name of Mike. 
+        //public static Customer RunProblem3(List<Customer> customers)
+        //{
+        //    //code
 
-            //return
+        //    //return
 
-        }
-        #endregion
+        //}
+        //#endregion
 
-        #region Problem 4
-        //(5 points) Problem 4
-        //Using LINQ, write a method that takes in a list of customers and returns the customer who has an id of 3. 
-        //Then, update that customer's first name and last name to completely different names and return the newly updated customer from the method.
-        public static Customer RunProblem4(List<Customer> customers)
-        {
-            //code
+        //#region Problem 4
+        ////(5 points) Problem 4
+        ////Using LINQ, write a method that takes in a list of customers and returns the customer who has an id of 3. 
+        ////Then, update that customer's first name and last name to completely different names and return the newly updated customer from the method.
+        //public static Customer RunProblem4(List<Customer> customers)
+        //{
+        //    //code
 
-            //return
+        //    //return
 
-        }
-        #endregion
+        //}
+        //#endregion
 
-        #region Problem 5
-        //(5 points) Problem 5
-        //Using LINQ, write a method that calculates the class grade average after dropping the lowest grade for each student.
-        //The method should take in a list of strings of grades (e.g., one string might be "90,100,82,89,55"), 
-        //drops the lowest grade from each string, averages the rest of the grades from that string, then averages the averages.
-        //Expected output: 86.125
-        public static double RunProblem5(List<string> classGrades)
-        {
-            //code
+        //#region Problem 5
+        ////(5 points) Problem 5
+        ////Using LINQ, write a method that calculates the class grade average after dropping the lowest grade for each student.
+        ////The method should take in a list of strings of grades (e.g., one string might be "90,100,82,89,55"), 
+        ////drops the lowest grade from each string, averages the rest of the grades from that string, then averages the averages.
+        ////Expected output: 86.125
+        //public static double RunProblem5(List<string> classGrades)
+        //{
+        //    //code
 
-            //return
+        //    //return
 
-        }
-        #endregion
+        //}
+        //#endregion
 
-        #region Bonus Problem 1
-        //(5 points) Bonus Problem 1
-        //Write a method that takes in a string of letters(i.e. “Terrill”) 
-        //and returns an alphabetically ordered string corresponding to the letter frequency(i.e. "E1I1L2R2T1")
-        public static string RunBonusProblem1(string word)
-        {
-            //code
+        //#region Bonus Problem 1
+        ////(5 points) Bonus Problem 1
+        ////Write a method that takes in a string of letters(i.e. “Terrill”) 
+        ////and returns an alphabetically ordered string corresponding to the letter frequency(i.e. "E1I1L2R2T1")
+        //public static string RunBonusProblem1(string word)
+        //{
+        //    //code
 
-            //return
+        //    //return
 
-        }
-        #endregion
+        //}
+        //#endregion
     }
 }
--- PracticeProblemsLINQ/Program.cs
+++ PracticeProblemsLINQ/Program.cs
@@ -12,35 +12,35 @@ namespace PracticeProblemsLINQ
         {
             //Problem 1
             List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+            LinqProblems.RunProblem1(words);
+
+            ////Problem 2
+            //List<string> names = new List<string>() { "Mike", "Brad", "Nevin", "Ian", "Mike" };
 
 
-            //Problem 2
-            List<string> names = new List<string>() { "Mike", "Brad", "Nevin", "Ian", "Mike" };
+            ////Problem 3 & Problem 4
+            //List<Customer> customers = new List<Customer>()
+            //{
+            //    new Customer(1, "Mike", "Rodgers"),
+            //    new Customer(2, "Nick", "Allen"),
+            //    new Customer(3, "Jason", "Ryan"),
+            //    new Customer(4, "Dan", "Laffey")
+            //};
 
 
-            //Problem 3 & Problem 4
-            List<Customer> customers = new List<Customer>()
-            {
-                new Customer(1, "Mike", "Rodgers"),
-                new Customer(2, "Nick", "Allen"),
-                new Customer(3, "Jason", "Ryan"),
-                new Customer(4, "Dan", "Laffey")
-            };
+            ////Problem 5
+            //List<string> classGrades = new List<string>()
+            //{
+            //    "80,100,92,89,65", 
+            //    "93,81,78,84,69",
+            //    "73,88,83,99,64",
+            //    "98,100,66,74,55"
+            //};
 
 
-            //Problem 5
-            List<string> classGrades = new List<string>()
-            {
-                "80,100,92,89,65", 
-                "93,81,78,84,69",
-                "73,88,83,99,64",
-                "98,100,66,74,55"
-            };
-
-
-            //--------------------
-            //Bonus Problem 1
-            string letters = "terrill";
+            ////--------------------
+            ////Bonus Problem 1
+            //string letters = "terrill";
 
         }
     }
